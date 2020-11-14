e0e9a5821939a325f332dde9330fd465b3b947f5
Itsakaseru <32617101+Itsakaseru@users.noreply.github.com>
2020-11-14 20:21:05 +0700

    Add camera controller

---
+++ Assets/Scripts/Controller/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+
+    public float rotateSpeed = 5;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * rotateSpeed, Vector3.up) * offset;
+        transform.position = player.transform.position + offset;
+        transform.LookAt(player.transform.position);
+    }
+}
