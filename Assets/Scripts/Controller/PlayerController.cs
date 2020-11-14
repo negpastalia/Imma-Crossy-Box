fd5b40149eb4b1eb8c833b14baede4381618218c
Itsakaseru <32617101+Itsakaseru@users.noreply.github.com>
2020-11-14 20:21:18 +0700

    Add player controller

---
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W) && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(-transform.forward * 500);
+        }
+
+        if (Input.GetKey(KeyCode.S) && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(transform.forward * 500);
+        }
+
+        if (Input.GetKey(KeyCode.Q) && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(transform.right * 500);
+        }
+
+        if (Input.GetKey(KeyCode.E) && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(-transform.right * 500);
+        }
+
+        if (Input.GetKey(KeyCode.A) && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(transform.up * 500);
+        }
+
+        if (Input.GetKey(KeyCode.D) && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(-transform.up * 500);
+        }
+    }
+
+}
