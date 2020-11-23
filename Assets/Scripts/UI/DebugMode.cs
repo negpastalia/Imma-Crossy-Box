3bdcf23c6ed4e98f4211de0f21659ef6b25f80c1
Itsakaseru <32617101+Itsakaseru@users.noreply.github.com>
2020-11-24 00:40:23 +0700

    Add debug menu for timescale control

---
+++ Assets/Scripts/UI/DebugMode.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugMode : MonoBehaviour
+{
+    public GameInfo gameInfo;
+    public Toggle debugMode;
+
+    private void Start()
+    {
+        if (gameInfo.DebugMode) debugMode.isOn = true;
+        else debugMode.isOn = false;
+    }
+    public void Check()
+    {
+        if (debugMode.isOn)
+        {
+            EnableDebugMode();
+        }
+        if (!debugMode.isOn)
+        {
+            DisableDebugMode();
+        }
+    }
+
+    void EnableDebugMode()
+    {
+        gameInfo.DebugMode = true;
+    }
+
+    void DisableDebugMode()
+    {
+        gameInfo.DebugMode = false;
+    }
+}
