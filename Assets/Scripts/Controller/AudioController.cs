ea80de3057c8323ecd94f981f1c77913a9eb8b71
Itsakaseru <32617101+Itsakaseru@users.noreply.github.com>
2020-11-24 00:39:19 +0700

    Autoplay bgm music according to scene type

---
+++ Assets/Scripts/Controller/AudioController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    public AudioManager audioManager;
+
+    public int status;
+
+    void Start()
+    {
+        audioManager = GameObject.Find("AudioManager").GetComponent<AudioManager>();
+
+        switch (status)
+        {
+            case -1:
+                audioManager.PlayBGMMainMenu();
+                break;
+
+            case 1:
+                audioManager.PlayBGMStage1();
+                break;
+
+            case 2:
+                audioManager.PlayBGMStage2();
+                break;
+
+            case 3:
+                audioManager.PlayBGMStage3();
+                break;
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Menu/AudioManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource bgm;
+
+    public AudioClip bgmMainMenu;
+    public AudioClip bgmStage1;
+    public AudioClip bgmStage2;
+    public AudioClip bgmStage3;
+
+    static bool spawnedAudioManager = false;
+
+    private void Start()
+    {
+        if (!spawnedAudioManager)
+        {
+            DontDestroyOnLoad(gameObject);
+            spawnedAudioManager = true;
+        }
+    }
+
+    public void PlayBGMMainMenu()
+    {
+        bgm.clip = bgmMainMenu;
+        bgm.Play();
+    }
+
+    public void PlayBGMStage1()
+    {
+        bgm.clip = bgmStage1;
+        bgm.Play();
+    }
+
+    public void PlayBGMStage2()
+    {
+        bgm.clip = bgmStage2;
+        bgm.Play();
+    }
+
+    public void PlayBGMStage3()
+    {
+        bgm.clip = bgmStage3;
+        bgm.Play();
+    }
+
+
+}
